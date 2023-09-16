926fc3998daa8d5cb68842efebacd2044bdbc980
FrancescaRuggiero <francescapia99@live.it>
2023-09-16 18:29:51 +0200

    FILES: docker, kubernetes funzionanti, mancanti di configmap

--- src/main/java/unisannio/assd/gruppo3/RTC/App.java
+++ src/main/java/unisannio/assd/gruppo3/RTC/App.java
@@ -1,19 +1,25 @@
 package unisannio.assd.gruppo3.RTC;
 
-
 //{"id_disp":"AAA-1834-LOAD-S-2","timestamp":"1694797367000","disp_type":"load","value":30000, "intervalTime":10000}
 import comm.AppKafkaConsumer;
 import comm.AppKafkaProducer;
 import utils.ConnectionDB;
+
+import java.util.Map;
+
 public class App {
     //"172.31.3.218:31200"
     //"localhost:9092"
-    private static String APP_ID = "app2024";
-    private static String BROKERS = "172.31.3.218:31200";
+
+    static Map<String, String> env = System.getenv();
+
+    private static String APP_ID = env.get("APP_ID");
+    private static String BROKERS = env.get("BROKERS");
+    private static String MONGO_URI = env.get("MONGO_URI");
 
     public static void main(String[] args) throws InterruptedException {
-    	ConnectionDB.getInstance().connectToDB();
-    	AppKafkaProducer producer = new AppKafkaProducer(APP_ID, BROKERS);
+        ConnectionDB.getInstance().connectToDB(MONGO_URI);
+        AppKafkaProducer.getInstance();
         AppKafkaConsumer appConsumer = new AppKafkaConsumer(APP_ID, BROKERS);
         appConsumer.start();
     }
--- src/main/java/utils/ConnectionDB.java
+++ src/main/java/utils/ConnectionDB.java
@@ -13,34 +13,34 @@ import com.mongodb.client.MongoIterable;
 
 
 public class ConnectionDB {
-	 private static ConnectionDB instance = null;
-	 private MongoDatabase database;
+	private static ConnectionDB instance = null;
+	private MongoDatabase database;
 	public ConnectionDB() {}
 	public static ConnectionDB getInstance() {
-        if (instance == null) {
-            instance = new ConnectionDB();
-        }
-        return instance;
-    }
-
-	    public void connectToDB() {
-	        // Replace the placeholder with your MongoDB deployment's connection string
-	        String uri = "mongodb://127.0.0.1:27017";
-
-	        MongoClient mongoClient = MongoClients.create(uri);
-	        	try {
-	                database = mongoClient.getDatabase("RTCStorage");
-	                if(database.getCollection("LoadDatas") == null) database.createCollection("LoadDatas");
-	            } catch (MongoCommandException e) {
-	                System.err.println("Already exist: " + e.getMessage());
-	            }
-	            System.out.println("Connessione effettuata al db: "+database.getName());
-	    }
-	    
-		public MongoDatabase getDatabase() {
-			return database;
+		if (instance == null) {
+			instance = new ConnectionDB();
 		}
-	   
-	 
-	
+		return instance;
+	}
+
+	public void connectToDB(String uri) {
+		// Replace the placeholder with your MongoDB deployment's connection string
+		// String uri = "mongodb://127.0.0.1:27017";
+
+		MongoClient mongoClient = MongoClients.create(uri);
+		try {
+			database = mongoClient.getDatabase("RTCStorage");
+			if(database.getCollection("LoadDatas") == null) database.createCollection("LoadDatas");
+		} catch (MongoCommandException e) {
+			System.err.println("Already exist: " + e.getMessage());
+		}
+		System.out.println("Connessione effettuata al db: "+database.getName());
+	}
+
+	public MongoDatabase getDatabase() {
+		return database;
+	}
+
+
+
 }
